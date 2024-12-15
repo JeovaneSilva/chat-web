2292fb0bd9960ec0f798c3b9fde38fd801c7020c
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-12-15 18:23:29 -0300

    ajuste no next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '3333', // Porta onde o NestJS está rodando
-          pathname: '/uploads/profile_pictures/**', // Caminho para as imagens
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'official-hinda-jeovanesilva-b7372ca0.koyeb.app', // Substituindo pelo seu domínio de backend
+        port: '', // Deixe vazio se não usar uma porta específica
+        pathname: '/uploads/profile_pictures/**', // Caminho para as imagens
+      },
+    ],
+  },
+};
+
+export default nextConfig;
